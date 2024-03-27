146f5af668f837fe2e45f34f15be486f29a60002
ayaabb <ayaabbas011@gmail.com>
2024-03-27 21:16:07 +0200

    edited the main

--- __main__.py
+++ __main__.py
@@ -1,9 +1,11 @@
 from Load_balancer.load_balancer import LoadBalancer
+from database.create_tickets import create_tickets
 from request_stream.request_stream import RequestGenerator
 from server.server_stream import generate_servers
 
 
 def main():
+    create_tickets()
     request_generator = RequestGenerator()
     servers = generate_servers(5,"database/tickets.json")
     load_balancer = LoadBalancer(request_generator, servers)
